6364e6c50fb1ac30aaf606fedb55944a21227d6e
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-10 14:59:46 +0100

    Finished Review model implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'restaurants#index'
-  resources :restaurants, only: [:index, :show, :new, :create]
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
