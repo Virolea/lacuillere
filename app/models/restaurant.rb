11eb3187c8110ccf82943ffed98fc9f330bfddf1
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-10 14:32:04 +0100

    CRUD added to Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
+  CATEGORIES = %w(chinese italian japanese french belgian)
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian), allow_nil: false }
+  validates :category, inclusion: { in: CATEGORIES, allow_nil: false }
 end
